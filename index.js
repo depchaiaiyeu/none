34f3512629276ad9f11777a66a9adf4a60df6807
Vũ Xuân Kiên 👑 <Vxkiue@gmail.com>
2025-06-02 03:39:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -175,9 +175,9 @@ bot.on('message', async (msg) => {
     const args = text.split(/\s+/).slice(1)
     let [method, target, timeStr, threadsStr, proxyFile, rateStr] = args
     const time = parseInt(timeStr) || 25
-    const threads = parseInt(threadsStr) || 12
+    const threads = parseInt(threadsStr) || 10
     const proxy = proxyFile || './prx.txt'
-    const rate = parseInt(rateStr) || 64
+    const rate = parseInt(rateStr) || 25
 
 
     if (!target) {
